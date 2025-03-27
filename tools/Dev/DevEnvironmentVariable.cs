lancra/gsi

Add local build flag to dotnet target

tools/Dev/DevEnvironmentVariable.cs
@@ -1,0 +1,41 @@
+namespace GitStatusInteractive.Dev;
+
+internal sealed class DevEnvironmentVariable
+{
+    private const string Prefix = "GSI_";
+
+    private static readonly string[] TrueValues =
+    [
+        "1",
+        "on",
+        "true",
+        "yes",
+    ];
+
+    private bool _hydratedValue;
+    private string? _value;
+
+    private DevEnvironmentVariable(string name) => Name = Prefix + name;
+
+    public string Name { get; }
+
+    public string? Value
+    {
+        get
+        {
+            if (!_hydratedValue)
+            {
+                _value = Environment.GetEnvironmentVariable(Name);
+                _hydratedValue = true;
+            }
+
+            return _value;
+        }
+    }
+
+    public static implicit operator DevEnvironmentVariable(string name) => new(name);
+
+    public static implicit operator bool(DevEnvironmentVariable variable)
+        => variable.Value is not null &&
+        TrueValues.Contains(variable.Value, StringComparer.OrdinalIgnoreCase);
+}

tools/Dev/EnvironmentVariables.cs
@@ -1,0 +1,6 @@
+namespace GitStatusInteractive.Dev;
+
+internal static class EnvironmentVariables
+{
+    public static readonly DevEnvironmentVariable LocalBuild = "LOCAL_BUILD";
+}

lancra/gint

tools/Dev/DevEnvironmentVariable.cs
@@ -1,0 +1,41 @@
+namespace Gint.Dev;
+
+internal sealed class DevEnvironmentVariable
+{
+    private const string Prefix = "GINT_";
+
+    private static readonly string[] TrueValues =
+    [
+        "1",
+        "on",
+        "true",
+        "yes",
+    ];
+
+    private bool _hydratedValue;
+    private string? _value;
+
+    private DevEnvironmentVariable(string name) => Name = Prefix + name;
+
+    public string Name { get; }
+
+    public string? Value
+    {
+        get
+        {
+            if (!_hydratedValue)
+            {
+                _value = Environment.GetEnvironmentVariable(Name);
+                _hydratedValue = true;
+            }
+
+            return _value;
+        }
+    }
+
+    public static implicit operator DevEnvironmentVariable(string name) => new(name);
+
+    public static implicit operator bool(DevEnvironmentVariable variable)
+        => variable.Value is not null &&
+        TrueValues.Contains(variable.Value, StringComparer.OrdinalIgnoreCase);
+}

tools/Dev/EnvironmentVariables.cs
@@ -1,0 +1,6 @@
+namespace Gint.Dev;
+
+internal static class EnvironmentVariables
+{
+    public static readonly DevEnvironmentVariable LocalBuild = "LOCAL_BUILD";
+}