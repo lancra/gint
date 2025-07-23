07c8728895de8b485af44be900315d77c78bf945
Lance Craig <lancra@outlook.com>
2025-07-23 06:23:20 -0400

    Move solution filter creation into target

--- tools/Dev/Targets/Build/SolutionTarget.cs
+++ tools/Dev/Targets/Build/SolutionTarget.cs
@@ -1,7 +1,19 @@
+using System.Reflection;
+using System.Text.Json;
+
 namespace Gint.Dev.Targets.Build;
 
 internal sealed class SolutionTarget : ITarget
 {
+    private const string SolutionName = "Gint";
+
+    private static readonly JsonSerializerOptions SerializerOptions =
+        new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true,
+        };
+
     public void Setup(Bullseye.Targets targets)
         => targets.Add(
             TargetKeys.Solution,
@@ -9,6 +21,38 @@ internal sealed class SolutionTarget : ITarget
             Execute);
 
     private static async Task Execute()
-        => await PowerShell.Run("-Command scripts/create-solution-filter.ps1")
-        .ConfigureAwait(false);
+    {
+        var rootDirectory = Directory.GetCurrentDirectory();
+        var solutionFilterPath = Path.Combine(rootDirectory, ArtifactPaths.Root, $"{SolutionName}.slnf");
+
+        var assemblyName = Assembly.GetExecutingAssembly()
+            .GetName();
+        var projectEnumerationOptions = new EnumerationOptions
+        {
+            MaxRecursionDepth = 2,
+            RecurseSubdirectories = true,
+        };
+        var projectPaths = Directory.GetFiles(rootDirectory, "*.csproj", projectEnumerationOptions)
+            .Where(path => Path.GetFileNameWithoutExtension(path) != assemblyName.Name)
+            .Select(path => path.Replace($"{rootDirectory}\\", string.Empty))
+            .ToArray();
+
+        var solutionRelativePath = Path.Combine("..", $"{SolutionName}.slnx");
+        var solutionFilter = new SolutionFilter(new(solutionRelativePath, projectPaths));
+        using var fileStream = File.OpenWrite(solutionFilterPath);
+        await JsonSerializer.SerializeAsync(fileStream, solutionFilter, SerializerOptions)
+            .ConfigureAwait(false);
+    }
+
+    private sealed class SolutionFilter(Solution solution)
+    {
+        public Solution Solution { get; } = solution;
+    }
+
+    private sealed class Solution(string path, IReadOnlyCollection<string> projects)
+    {
+        public string Path { get; } = path;
+
+        public IReadOnlyCollection<string> Projects { get; } = projects;
+    }
 }
